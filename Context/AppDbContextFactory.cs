4632484bb4fb0fab7416cb8135272dc5cbd00111
recepbaykara <recep.bykr.65@gmail.com>
2025-12-29 01:01:17 +0300

    factory update

--- Context/AppDbContextFactory.cs
+++ Context/AppDbContextFactory.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using StockMgmt.Context;
 
 public class AppDbContextFactory 
     : IDesignTimeDbContextFactory<AppDbContext>
